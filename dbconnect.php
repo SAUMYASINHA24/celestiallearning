b79e069344f371b9c7d2219f843c2025b16d271a
vishal <vishalkale151071@gmail.com>
2020-11-06 20:29:34 +0530

    OOP Database connection added.

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,48 @@
 <?php 
-    $servername = "sql12.freemysqlhosting.net";
-    $username = "sql12374857";
-    $password = "lNXpUaAXvw";
+    class Database {
+        private $_connection;
+        private static $_instance; //The single instance
+        private $_host = "sql12.freemysqlhosting.net";
+        private $_username = "sql12374857";
+        private $_password = "lNXpUaAXvw";
+        private $_database = "sql12374857";
     
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password);
-
-    // Check connection
-    if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+        /*
+        Get an instance of the Database
+        @return Instance
+        */
+        public static function getInstance() {
+            if(!self::$_instance) { // If no instance then make one
+                self::$_instance = new self();
+            }
+            return self::$_instance;
+        }
+    
+        // Constructor
+        private function __construct() {
+            $this->_connection = new mysqli($this->_host, $this->_username, 
+                $this->_password, $this->_database);
+        
+            // Error handling
+            if(mysqli_connect_error()) {
+                trigger_error("Failed to conencto to MySQL: " . mysql_connect_error(),
+                     E_USER_ERROR);
+            }
+        }
+    
+        // Magic method clone is empty to prevent duplication of connection
+        private function __clone() { }
+    
+        // Get mysqli connection
+        public function getConnection() {
+            return $this->_connection;
+        }
     }
-    echo "Connected successfully";
+
+    $db = Database::getInstance();
+    $mysqli = $db->getConnection();
+    $sql_query = "SHOW tables";
+    $result = $mysqli->query($sql_query);
+    
+    echo "Result : $result";
 ?>
