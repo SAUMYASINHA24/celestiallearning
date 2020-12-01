099b8dc2da53d00870819ef5e0fe7250b5a44b27
phpmysql <saumyasinha38@gmail.com>
2020-12-01 21:19:40 +0530

    Final module

--- SMTPMail.php
+++ SMTPMail.php
@@ -66,8 +66,8 @@
             $mail->isSMTP();                                            // Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = "saumyasinhatest@gmail.com";                     // SMTP username
-            $mail->Password   = "Jaimatadi@123";                               // SMTP password
+            $mail->Username   = "";                     // SMTP username
+            $mail->Password   = "";                               // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
             // goes in database
